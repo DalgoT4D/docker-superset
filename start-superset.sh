9d60b48441ceceab3f6ec3b28b4e7f9d773bb241
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-06-30 23:34:02 +0530

    move celery programs into docker-compose

--- start-superset.sh
+++ start-superset.sh
@@ -4,9 +4,9 @@ set -o allexport
 source superset.env
 set +o allexport
 
-docker exec -itd docker-superset-superset-1 celery --app=superset.tasks.celery_app:app worker --pool=prefork -O fair -c 4
-docker exec -itd docker-superset-superset-1 celery --app=superset.tasks.celery_app:app beat -s /app/superset_home/celerybeat_schedule
-docker exec -itd docker-superset-superset-1 celery --app=superset.tasks.celery_app:app flower
+# docker exec -itd docker-superset-superset-1 celery --app=superset.tasks.celery_app:app worker --pool=prefork -O fair -c 4
+# docker exec -itd docker-superset-superset-1 celery --app=superset.tasks.celery_app:app beat -s /app/superset_home/celerybeat_schedule
+# docker exec -itd docker-superset-superset-1 celery --app=superset.tasks.celery_app:app flower
 
 docker exec -it docker-superset-superset-1 superset db upgrade
 docker exec -it docker-superset-superset-1 superset fab create-admin --username admin --password ${SUPERSET_ADMIN_PASSWORD} --firstname Superset --lastname Admin --email ${SUPERSET_ADMIN_EMAIL}
