2e34560683d47d8d567c877b8bfdc15aba3cb98b
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-07-02 15:24:14 +0530

    specify username for admin user

--- start-superset.sh
+++ start-superset.sh
@@ -5,5 +5,5 @@ source superset.env
 set +o allexport
 
 docker exec -it superset-oauth superset db upgrade
-docker exec -it superset-oauth superset fab create-admin --username admin --password ${SUPERSET_ADMIN_PASSWORD} --firstname Superset --lastname Admin --email ${SUPERSET_ADMIN_EMAIL}
+docker exec -it superset-oauth superset fab create-admin --username ${SUPERSET_ADMIN_USERNAME} --password ${SUPERSET_ADMIN_PASSWORD} --firstname Superset --lastname Admin --email ${SUPERSET_ADMIN_EMAIL}
 docker exec -it superset-oauth superset init
