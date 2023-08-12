1a6c888bc3a49814702762f81832f8379cd61317
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-08-12 20:12:01 +0530

    read from json instead of csv add role to input json

--- scripts/uploadusers.py
+++ scripts/uploadusers.py
@@ -2,7 +2,7 @@
 this script will be copied into and run from within the docker container
 """
 import argparse
-from csv import DictReader
+import json
 
 import logging
 
@@ -14,16 +14,12 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-try:
-    from custom_user import CustomUser
-except ImportError:
-    from superset.custom_user import CustomUser
-
+from superset.custom_user import CustomUser
 from sqlalchemy import create_engine, select
 
 engine = create_engine("postgresql://postgres:password@db/superset")
-
 from sqlalchemy.orm import sessionmaker
+from flask_appbuilder.security.sqla.models import Role
 
 Session = sessionmaker(bind=engine)
 
@@ -31,32 +27,30 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "-f",
     "--file",
-    default="/host_data/users.csv",
-    help="CSV file containing users to upsert, usually from /host_data/",
+    default="/host_data/users.json",
+    help="JSON file containing users to upsert, usually from /host_data/",
 )
 args = parser.parse_args()
 
 
-def read_csv(filename: str):
-    """reads and returns all objects in the csv specified by the user"""
-    retval = []
+def read_json(filename: str):
+    """reads and returns all objects in the json specified by the user"""
     with open(filename, "r", encoding="utf-8") as infile:
-        reader = DictReader(infile)
-        logger.info("reading users from %s", filename)
-        for obj in reader:
-            retval.append(obj)
-            logger.info(obj)
-        logger.info("finished reading users from %s", filename)
-    return retval
+        return json.load(infile)
 
 
 # == start
-# we treat `email` as the comparison key
-users_to_upload = read_csv(args.file)
+# load roles from database
 session = Session()
+DBROLES = {}
+for rr in session.scalars(select(Role)).all():
+    DBROLES[rr.name] = rr
 
-for csvuser in users_to_upload:
-    email = csvuser["email"].strip().lower()
+# we treat `email` as the comparison key
+users_to_upload = read_json(args.file)
+
+for jsonuser in users_to_upload["users"]:
+    email = jsonuser["email"].strip().lower()
     # first see if the user is already in the db, check by email address
     query = select(CustomUser).where(CustomUser.email == email)
     result = session.scalars(query).all()
@@ -72,11 +66,12 @@ for csvuser in users_to_upload:
         session.add(user)
         logger.info("creating user with email %s", email)
 
-    user.username = csvuser["username"]
-    user.first_name = csvuser["first_name"]
-    user.last_name = csvuser["last_name"]
+    user.username = jsonuser["username"]
+    user.first_name = jsonuser["first_name"]
+    user.last_name = jsonuser["last_name"]
     user.email = email
-    user.blob = csvuser["blob"]
+    user.roles = [DBROLES[role_str] for role_str in jsonuser["roles"]]
+    user.blob = json.dumps(jsonuser["blob"])
 
     logger.info(
         "writing user email=%s username=%s blob=%s",
