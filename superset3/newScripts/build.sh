55a7643d9953d6b43427eea6d249cae24cdcf2df
himanshudube97 <himanshu.dube13@gmail.com>
2024-08-14 14:58:17 +0530

    removed the old scripts by himanshu

--- superset3/newScripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define the output image name and tag
-OUTPUT_IMAGE="tech4dev/superset:0.4"
-
-# Build the Docker image
-docker build --tag $OUTPUT_IMAGE .
-
-echo "Docker image $OUTPUT_IMAGE built successfully."
--- superset3/newScripts/push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define the output image name and tag
-OUTPUT_IMAGE="tech4dev/superset:0.4"
-
-# Push the Docker image to DockerHub
-docker push $OUTPUT_IMAGE
-
-echo "Docker image $OUTPUT_IMAGE pushed to DockerHub successfully."
