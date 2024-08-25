51391d636a1b7d83b31176cb03d10835ad4ac12e
himanshudube97 <himanshu.dube13@gmail.com>
2024-08-25 16:39:16 +0530

    some naming changes

--- gensuperset/make-client/generate-make-client.sh
+++ gensuperset/make-client/generate-make-client.sh
@@ -65,9 +65,9 @@ set -o allexport
 source superset.env
 set +o allexport
 
-docker exec -it ${SUPERSET_CONTAINER_NAME} superset db upgrade
-docker exec -it ${SUPERSET_CONTAINER_NAME} superset fab create-admin --username \${SUPERSET_ADMIN_USERNAME} --password \${SUPERSET_ADMIN_PASSWORD} --firstname Superset --lastname Admin --email \${SUPERSET_ADMIN_EMAIL}
-docker exec -it ${SUPERSET_CONTAINER_NAME} superset init
+docker exec -it superset-${SUPERSET_CONTAINER_NAME} superset db upgrade
+docker exec -it superset-${SUPERSET_CONTAINER_NAME} superset fab create-admin --username \${SUPERSET_ADMIN_USERNAME} --password \${SUPERSET_ADMIN_PASSWORD} --firstname Superset --lastname Admin --email \${SUPERSET_ADMIN_EMAIL}
+docker exec -it superset-${SUPERSET_CONTAINER_NAME} superset init
 EOF
 
 # Make the script executable
